9f055e74034a6b1bcee65ca839595cd1e117d139
Alex Queiroz <asandroq@gmail.com>
2018-06-04 17:28:42 +0200

    Generate each top-level function as a string before writing

--- src/IRTS/CodegenGambit.hs
+++ src/IRTS/CodegenGambit.hs
@@ -49,130 +49,91 @@ genInitialState = traverse (uncurry go)
                                           pure (name, cnt)
 
 codegen :: Name -> LDecl -> GenState ()
-codegen name (LFun _ _ args expr) = do genPrint "(define ("
-                                       genPrint $ quoteSym name
-                                       genArgs args
-                                       genPrint ") "
-                                       genExpr expr
-                                       genPrint ")\n\n"
+codegen name (LFun _ _ args expr) = do let name' = quoteSym name
+                                       let args' = genArgs args
+                                       expr' <- genExpr expr
+                                       genPrint $ "(define (" ++ name' ++ args' ++ ") " ++ expr' ++ ")\n\n"
 codegen _ _ = pure ()
 
-genExpr :: LExp -> GenState ()
-genExpr (LV name) = genPrint $ quoteSym name
+genExpr :: LExp -> GenState String
+genExpr (LV name) = pure $ quoteSym name
 genExpr (LApp _ (LV fn) args) = do ctx <- gets genCtx
                                    case Map.lookup fn ctx of
                                      Just (LConstructor _ _ ary) -> genApply fn ary args
                                      Just (LFun _ _ as _) -> genApply fn (length as) args
                                      Nothing -> genApply fn (length args) args
-genExpr (LApp _ f _) = do genPrint "(error \"Impossible application of expression \""
-                          genExpr f
-                          genPrint "\")"
-genExpr (LLazyApp fn args) = do genPrint "(delay "
-                                genExpr (LApp False (LV fn) args)
-                                genPrint ")"
-genExpr (LLazyExp expr) = do genPrint "(delay "
-                             genExpr expr
-                             genPrint ")"
+genExpr (LApp _ f _) = do f' <- genExpr f
+                          pure $ "(error \"Impossible application of expression " ++ f' ++ ")"
+genExpr (LLazyApp fn args) = do expr <- genExpr (LApp False (LV fn) args)
+                                pure $ "(delay "++ expr ++ ")"
+genExpr (LLazyExp expr) = do expr' <- genExpr expr
+                             pure $ "(delay " ++ expr' ++ ")"
 genExpr (LForce (LLazyApp n args)) = genExpr (LApp False (LV n) args)
-genExpr (LForce expr) = do genPrint "(force "
-                           genExpr expr
-                           genPrint ")"
-genExpr (LLet name expr body) = do genPrint "(let (("
-                                   genPrint $ quoteSym name
-                                   genPrint " "
-                                   genExpr expr
-                                   genPrint ")) "
-                                   genExpr body
-                                   genPrint ")"
-genExpr (LLam args body) = do genPrint "(lambda ("
-                              genArgs args
-                              genPrint ") "
-                              genExpr body
-                              genPrint ")"
-genExpr (LProj expr fld) = do genPrint "(##vector-ref "
-                              genExpr expr
-                              genPrint " "
-                              genPrint $ show (fld + 1)
-                              genPrint ")"
-genExpr (LCon _ _ name args) = do genPrint "(vector '"
-                                  genPrint $ quoteSym name
-                                  genExprs args
-                                  genPrint ")"
-genExpr (LCase _ expr alts) = do genPrint "(case "
-                                 if any isConCase alts
-                                    then do genPrint "(##vector-ref "
-                                            genExpr expr
-                                            genPrint " 0)"
-                                    else genExpr expr
-                                 traverse_ (\a -> genPrint " " >> genAlt expr a) alts
-                                 genPrint ")"
+genExpr (LForce expr) = do expr' <- genExpr expr
+                           pure $ "(force " ++ expr' ++ ")"
+genExpr (LLet name expr body) = do let name' = quoteSym name
+                                   expr' <- genExpr expr
+                                   body' <- genExpr body
+                                   pure $ "(let ((" ++ name' ++ " " ++ expr' ++ ")) " ++ body' ++ ")"
+genExpr (LLam args body) = do let args' = genArgs args
+                              body' <- genExpr body
+                              pure $ "(lambda (" ++ args' ++ ") " ++ body' ++ ")"
+genExpr (LProj expr fld) = do expr' <- genExpr expr
+                              let fld' = show (fld + 1)
+                              pure $ "(##vector-ref " ++ expr' ++ " " ++ fld' ++ ")"
+genExpr (LCon _ _ name args) = do let name' = quoteSym name
+                                  args' <- genExprs args
+                                  pure $ "(vector '" ++ name' ++ args' ++ ")"
+genExpr (LCase _ expr alts) = do expr' <- genExpr expr
+                                 alts' <- concat <$> traverse (genAlt expr) alts
+                                 pure $ "(case " ++
+                                      if any isConCase alts
+                                      then "(##vector-ref " ++ expr' ++ " 0)"
+                                      else expr'
+                                   ++ alts' ++ ")"
     where
       isConCase (LConCase _ _ _ _) = True
       isConCase _ = False
-genExpr (LConst cnt) = genPrint $ genConst cnt
-genExpr (LForeign r t _) = do genPrint "(error \"Foreign call to '"
-                              case t of
-                                FStr s -> genPrint s
-                                _ -> genPrint $ show t
-                              genPrint " ("
-                              genPrint $ show r
-                              genPrint ")"
-                              genPrint "' not implemented\")"
+genExpr (LConst cnt) = pure $ genConst cnt
+genExpr (LForeign _ t _) = do let name = case t of
+                                           FStr s -> s
+                                           _ -> show t
+                              pure $ "(error \"Foreign call to '" ++ name ++ "' not implemented\")"
 genExpr (LOp fn args) = genOp fn args
-genExpr LNothing = genPrint "(##void)"
-genExpr (LError msg) = do genPrint "(error \""
-                          genPrint msg
-                          genPrint "\")"
+genExpr LNothing = pure "(##void)"
+genExpr (LError msg) = pure $ "(error \"" ++ msg ++ "\")"
 
-genExprs :: [LExp] -> GenState ()
-genExprs = traverse_ (\e -> genPrint " " >> genExpr e)
+genExprs :: [LExp] -> GenState String
+genExprs exprs = concat . map (" " ++) <$> traverse genExpr exprs
 
-genApply :: Name -> Int -> [LExp] -> GenState ()
+genApply :: Name -> Int -> [LExp] -> GenState String
 genApply fn ary args = case compare ary (length args) of
-                         LT -> do genPrint "("
-                                  genApply fn ary (take ary args)
-                                  genExprs (drop ary args)
-                                  genPrint ")"
-                         EQ -> do genPrint "("
-                                  genPrint $ quoteSym fn
-                                  genExprs args
-                                  genPrint ")"
+                         LT -> do outer <- genApply fn ary (take ary args)
+                                  rest <- genExprs (drop ary args)
+                                  pure $ "(" ++ outer ++ rest ++ ")"
+                         EQ -> do args' <- genExprs args
+                                  pure $ "(" ++ quoteSym fn ++ args' ++ ")"
                          GT -> do extraArgs <- replicateM (ary - length args) genVar
-                                  genPrint "(lambda ("
-                                  genArgs extraArgs
-                                  genPrint ") "
-                                  genApply fn ary (args ++ (LV <$> extraArgs))
-                                  genPrint ")"
+                                  inner <- genApply fn ary (args ++ (LV <$> extraArgs))
+                                  pure $ "(lambda (" ++ genArgs extraArgs ++ ") " ++ inner ++ ")"
 
-genArgs :: [Name] -> GenState ()
-genArgs = traverse_ (\a -> genPrint (" " ++ quoteSym a))
+genArgs :: [Name] -> String
+genArgs = concatMap ((" " ++) . quoteSym)
 
-genAlt :: LExp -> LAlt -> GenState ()
-genAlt cnt (LConCase _ name args expr) = do genPrint "(("
-                                            genPrint $ quoteSym name
-                                            genPrint ") "
-                                            when (length args > 0) $ do
-                                              genPrint "(let ("
-                                              forM_ (zip args [1::Int ..]) $ \(a, i) -> do
-                                                genPrint "("
-                                                genPrint $ quoteSym a
-                                                genPrint " (##vector-ref "
-                                                genExpr cnt
-                                                genPrint " "
-                                                genPrint $ show i
-                                                genPrint "))"
-                                              genPrint ") "
-                                            genExpr expr
-                                            when (length args > 0) $ genPrint ")"
-                                            genPrint ")"
-genAlt _ (LConstCase cnt expr) = do genPrint "(("
-                                    genPrint $ genConst cnt
-                                    genPrint ") "
-                                    genExpr expr
-                                    genPrint ")"
-genAlt _ (LDefaultCase expr) = do genPrint "(else "
-                                  genExpr expr
-                                  genPrint ")"
+genAlt :: LExp -> LAlt -> GenState String
+genAlt cnt (LConCase _ name args expr) = do cnt' <- genExpr cnt
+                                            expr' <- genExpr expr
+                                            let body = if length args > 0
+                                                       then "(let (" ++ bindings cnt' ++ ") " ++ expr' ++ ")"
+                                                       else expr'
+                                            pure $ "((" ++ quoteSym name ++ ") " ++ body ++ ")"
+    where
+      bindings c = concatMap (binding c) (zip args [1::Int ..])
+      binding c (a, i) = "(" ++ quoteSym a ++ " (##vector-ref " ++ c ++ " " ++ show i ++ "))"
+genAlt _ (LConstCase cnt expr) = do expr' <- genExpr expr
+                                    pure $ "((" ++ genConst cnt ++ ") " ++ expr' ++ ")"
+genAlt _ (LDefaultCase expr) = do expr' <- genExpr expr
+                                  pure $ "(else " ++ expr' ++ ")"
 
 genConst :: Const -> String
 genConst (I i) = show i
@@ -186,19 +147,15 @@ genConst (B32 b) = show b
 genConst (B64 b) = show b
 genConst _ = "(error \"Constant type not implemented\")"
 
-genOp :: PrimFn -> [LExp] -> GenState ()
-genOp (LPlus (ATInt ITNative)) args = do genPrint "(##fx+"
-                                         genExprs args
-                                         genPrint ")"
-genOp (LMinus (ATInt ITNative)) args = do genPrint "(##fx-"
-                                          genExprs args
-                                          genPrint ")"
-genOp (LMinus (ATInt ITBig)) args = do genPrint "(-"
-                                       genExprs args
-                                       genPrint ")"
-genOp (LTimes (ATInt ITNative)) args = do genPrint "(##fx*"
-                                          genExprs args
-                                          genPrint ")"
+genOp :: PrimFn -> [LExp] -> GenState String
+genOp (LPlus (ATInt ITNative)) args = do args' <- genExprs args
+                                         pure $ "(##fx+" ++ args' ++ ")"
+genOp (LMinus (ATInt ITNative)) args = do args' <- genExprs args
+                                          pure $ "(##fx-" ++ args' ++ ")"
+genOp (LMinus (ATInt ITBig)) args = do args' <- genExprs args
+                                       pure $ "(-" ++ args' ++ ")"
+genOp (LTimes (ATInt ITNative)) args = do args' <- genExprs args
+                                          pure $ "(##fx*" ++ args' ++ ")"
 genOp (LEq aty) [l, r] = let op = case aty of
                                     ATInt (ITFixed _) -> "##fx="
                                     ATInt ITNative -> "##fx="
@@ -223,30 +180,21 @@ genOp (LSGt aty) [l, r] = let op = case aty of
                                     ATFloat -> "##fl>"
                          in genCompOp op l r
 genOp (LSExt _ _) [x] = genExpr x
-genOp (LIntStr ITChar) [x] = do genPrint "(make-string 1 "
-                                genExpr x
-                                genPrint ")"
-genOp (LIntStr _) [x] = do genPrint "(##number->string "
-                           genExpr x
-                           genPrint ")"
-genOp LStrConcat args = do genPrint "(##string-append"
-                           genExprs args
-                           genPrint ")"
-genOp LWriteStr [_, s] = do genPrint "(display "
-                            genExpr s
-                            genPrint ")"
-genOp fn _ = do genPrint "(error \"Primitive operation "
-                genPrint $ show fn
-                genPrint " not implemented yet\")"
+genOp (LIntStr ITChar) [x] = do x' <- genExpr x
+                                pure $ "(make-string 1 " ++ x' ++ ")"
+genOp (LIntStr _) [x] = do x' <- genExpr x
+                           pure $ "(##number->string " ++ x' ++ ")"
+genOp LStrConcat args = do args' <- genExprs args
+                           pure $  "(##string-append" ++ args' ++ ")"
+genOp LWriteStr [_, s] = do s' <- genExpr s
+                            pure $ "(display " ++ s' ++ ")"
+genOp fn _ = pure $ "(error \"Primitive operation " ++ show fn ++ " not implemented yet\")"
 
-genCompOp :: String -> LExp -> LExp -> GenState ()
-genCompOp op lhs rhs = do genPrint "(if ("
-                          genPrint op
-                          genPrint " "
-                          genExpr lhs
-                          genPrint " "
-                          genExpr rhs
-                          genPrint ") 1 0)"
+-- Idris's backend encodes booleans as integers
+genCompOp :: String -> LExp -> LExp -> GenState String
+genCompOp op lhs rhs = do lhs' <- genExpr lhs
+                          rhs' <- genExpr rhs
+                          pure $ "(if (" ++ op ++ " " ++ lhs' ++ " " ++ rhs' ++ ") 1 0)"
 
 quoteSym :: Name -> String
 quoteSym name = case name of
